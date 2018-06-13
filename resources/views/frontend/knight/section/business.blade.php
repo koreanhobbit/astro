fda29aa92c35630cb3ead503aed5c58fe58d7d98
ferry <ferryaze1@gmail.com>
2018-06-13 23:29:03 +0900

    change tagline

--- resources/views/frontend/knight/section/business.blade.php
+++ resources/views/frontend/knight/section/business.blade.php
@@ -1,7 +1,7 @@
 <section class="business-talking">
 	<!--business-talking-start-->
 	<div class="container">
-		<h2>We share your dreams</h2>
+		<h2>{{ $setting->tagline }}</h2>
 	</div>
 </section>
 <!--business-talking-end-->
